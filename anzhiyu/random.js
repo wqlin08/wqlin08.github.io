337e79dc2d55c5fc522d866bbadbe466be3b4c7b
wqlin08 <geo.wqlin@gmail.com>
2026-01-08 20:46:26 +0800

    Site updated: 2026-01-08 20:46:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/01/08/hello-world/"];function toRandomPost(){
+var posts=["2026/01/08/遥感/SAR/InSAR-01-ISCE2-Workflow/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
