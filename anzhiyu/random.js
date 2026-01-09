99d265514d29cb79fa9da79392caa972179ba3f4
wqlin08 <geo.wqlin@gmail.com>
2026-01-09 16:13:09 +0800

    Site updated: 2026-01-09 16:13:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/01/09/工具箱/程序/自动排版软件/","2026/01/09/工具箱/代码/羽化镶嵌/","2026/01/08/遥感/SAR/InSAR-01-ISCE2-Workflow/","2026/01/09/工具箱/代码/mapborn/"];function toRandomPost(){
+var posts=["2026/01/08/遥感/SAR/InSAR-01-ISCE2-Workflow/","2026/01/09/工具箱/代码/mapborn/","2026/01/09/工具箱/程序/自动排版软件/","2026/01/09/工具箱/代码/羽化镶嵌/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
